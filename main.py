cde3a593a0c26415079d1f47164a32d6c236bcbf
Bibek Acharya <92309375+Bibek04-bit@users.noreply.github.com>
2025-09-26 11:16:44 +0545

    updated

--- main.py
+++ main.py
@@ -30,53 +30,70 @@ class SignUp(unittest.TestCase):
                 EC.element_to_be_clickable((By.XPATH, '/html/body/div[3]/div[4]/div[3]/a/button'))
             ).click()
             time.sleep(2)
-           
+           #Frist Name
             fname = WebDriverWait(driver, 20).until(
                 EC.presence_of_element_located((By.NAME, 'firstName'))
             )
             fname.clear()
             fname.send_keys('Bibek')
             time.sleep(2)
-            
+            # Last Name
             lname = WebDriverWait(driver, 20).until(
                 EC.presence_of_element_located((By.NAME, 'lastName'))
             )
             lname.clear()
             lname.send_keys('Acharya')
             time.sleep(2)
-
+             #Email
             Email = WebDriverWait(driver, 20).until(
                 EC.presence_of_element_located((By.NAME, 'email'))
             )
             Email.clear()
-            Email.send_keys('t68210870@gmail.com ')
+            Email.send_keys(f"bibek{int(time.time())}@example.com")
             time.sleep(2)
-
+            #Phone number
             Phone = WebDriverWait(driver, 20).until(
                 EC.presence_of_element_located((By.NAME, 'phoneNumber'))
             )
             Phone.clear()
-            Phone.send_keys('9862906790')
+            Phone.send_keys(f"98{int(time.time()) % 10000000}")
             time.sleep(2)
-
+           #PassWOrd
             PassW = WebDriverWait(driver, 20).until(
                 EC.presence_of_element_located((By.NAME, 'password'))
             )
             PassW.clear()
-            PassW.send_keys('testmail100%')
-            time.sleep(10)
-
+            PassW.send_keys('Testmail100%')
+            time.sleep(3)
+           #Confirm Password
             CPassW = WebDriverWait(driver, 20).until(
                 EC.presence_of_element_located((By.NAME, 'confirmPassword'))
             )
             CPassW.clear()
-            CPassW.send_keys('testmail100%')
+            CPassW.send_keys('Testmail100%')
             time.sleep(2)
 
             WebDriverWait(driver, 20).until(
                 EC.element_to_be_clickable((By.XPATH, "/html/body/div[3]/div[4]/div/div/div/div[2]/form/div[4]/button"))
             ).click()
             time.sleep(2)
+
+            #Agency name
+            AgencyName = WebDriverWait(driver, 20).until(
+                EC.presence_of_element_located((By.NAME, 'agency_name'))
+            )
+            AgencyName.clear()
+            AgencyName.send_keys('HackYourDreams')
+            time.sleep(2)
+
+            #Role in agency
+            AgencyRole = WebDriverWait(driver, 20).until(
+                EC.presence_of_element_located((By.NAME, 'role_in_agency'))
+            )
+            AgencyRole.send_keys('Intern')
+            time.sleep(2)
+
+
 
 
 
